13316cd8916f0498d05839c09845b45679f8a73d
Jeremial <jeremial90@gmail.com>
2014-03-28 12:13:52 +0800

    update test

--- test/test.js
+++ test/test.js
@@ -2,10 +2,33 @@ var only = require('..');
 var metalSmith = require('metalsmith');
 
 describe('metalsmith-only', function(){
-    it('should filter attr', function(done){
+    it('should filter attr string', function(done){
         var metalsmith = metalSmith(__dirname);
 
         metalsmith.use(only('name title'));
+
+        var files = {
+            'a.md': {
+                name: 'a.name',
+                title: 'a.title',
+                content: 'a.content'
+            }
+        };
+
+        metalsmith.run(files, function(err, files){
+            should.not.exist(err);
+            files['a.md'].should.deep.equal({
+                name: 'a.name',
+                title: 'a.title'
+            });
+            done();
+        });
+    });
+
+    it('should filter attr array', function(done){
+        var metalsmith = metalSmith(__dirname);
+
+        metalsmith.use(only(['name', 'title']));
 
         var files = {
             'a.md': {
